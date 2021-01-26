d7db19beb354102a51da67a3dbda55a1f9e07359
EnriqueIzel2 <eleaoizel@gmail.com>
2021-01-26 17:54:52 -0400

    Final da aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-import db from "../db.json";
-import { Widget } from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import GithubCorner from "../src/components/GithubCorner";
-import { QuizBackground } from "../src/components/QuizBackground";
-import QuizLogo from "../src/components/QuizLogo";
+import db from '../db.json';
+import { Widget } from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GithubCorner from '../src/components/GithubCorner';
+import { QuizBackground } from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,24 +22,53 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura quiz</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>Timao</h1>
+            <h1 style={{ color: '#ffffff' }}>Timao</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function a(event) {
+              event.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                placeholder="Seu nome"
+                onChange={function namse(event) {
+                  setName(event.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1 style={{ color: '#ffffff' }}>Quizes da galera</h1>
           </Widget.Header>
           <Widget.Content>
             <p>lorem ipsum</p>
           </Widget.Content>
         </Widget>
-        <Widget>
-          <h1>Timao</h1>
-          <p>O melhor time</p>
-        </Widget>
         <Footer />
       </QuizContainer>
-      <GithubCorner projectUrl="https://github.com/EnriqueIzel2" />
+      <GithubCorner projectUrl="https://github.com/EnriqueIzel2/corinthians_quiz" />
     </QuizBackground>
   );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Quiz() {
+  return <div>Página quiz</div>;
+}
+
+export default Quiz;
